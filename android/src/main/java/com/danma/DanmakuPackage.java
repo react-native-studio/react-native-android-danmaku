9de7a2652537be4d5223d2d890fec3a7e5b12940
2534290808 <2534290808@qq.com>
2017-12-21 15:53:21 +0800

    适应rn0.47以上

--- android/src/main/java/com/danma/DanmakuPackage.java
+++ android/src/main/java/com/danma/DanmakuPackage.java
@@ -20,12 +20,6 @@ public class DanmakuPackage implements ReactPackage{
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
-
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Arrays.<ViewManager>asList(new DanmakuViewManager());
